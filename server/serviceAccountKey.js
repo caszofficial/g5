d9439ace726e1d954182b1eef574acdce4a8ac5d
Santiago <santiagozapata07@gmail.com>
2024-11-14 22:57:12 -0500

    env config

--- server/serviceAccountKey.js
+++ server/serviceAccountKey.js
@@ -1,17 +1,14 @@
-import {
-  PRIVATE_KEY_ID,
-  PRIVATE_KEY,
-  FIREBASE_CLIENT_ID,
-  FIREBASE_PROJECT_ID,
-} from "./envVar";
+import "dotenv/config.js";
+
+dotenv.config();
 
 export const firebaseConfig = {
   type: "service_account",
-  project_id: FIREBASE_PROJECT_ID,
-  private_key_id: PRIVATE_KEY_ID,
-  private_key: PRIVATE_KEY,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY,
   client_email: "firebase-adminsdk-i37rt@g5-test-7258e.iam.gserviceaccount.com",
-  client_id: FIREBASE_CLIENT_ID,
+  client_id: process.env.FIREBASE_CLIENT_ID,
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
