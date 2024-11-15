02ddf94d3e742d32fcf7c94476dc0ad549235955
Santiago <santiagozapata07@gmail.com>
2024-11-14 23:47:23 -0500

    fix deploy mistake

--- server/index.js
+++ server/index.js
@@ -6,31 +6,12 @@ import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(
-  cors({
-    origin: "https://g5-tawny.vercel.app",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-      "X-Requested-With",
-      "Accept",
-    ],
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 // Rutas
 app.use("/api", lotteryRoutes);
 app.use("/api/mercadopago", mpRoutes);
-
-app.options("/api/mercadopago/create_preference", (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://g5-tawny.vercel.app");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.sendStatus(200);
-});
 
 // Iniciar servidor
 app.listen(PORT, () => {
