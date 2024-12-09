8eebf89e2a176f35e9b67303e7437284c4e0e4d4
caszofficial <santiagozapata07@gmail.com>
2024-12-09 18:07:44 -0500

    styling extra

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer">
-      <div>
+      <div className="footer-list">
         <ul style={{ listStyle: "none", textAlign: "center" }}>
           <li>SDS</li>
           <li>CR 43A #7-50</li>
@@ -11,13 +11,8 @@ const Footer = () => {
           <li>sdsfabricadesuenos@gmail.com</li>
         </ul>
       </div>
-      <div style={{ height:"50px", margin:"20px 0px"}}>
-        <img
-          src="../../src/assets/SDS_Logo_blanco.png"
-          alt="No image Found"
-          width="100%"
-          height="100%"
-        />
+      <div className="footer-image">
+        <img src="../../src/assets/SDS_Logo_blanco.png" alt="No image Found" />
       </div>
     </footer>
   );
