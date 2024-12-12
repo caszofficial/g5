320cb0e8d91ce4d633f1c94bcb8f74dcd4c8b3f3
caszofficial <santiagozapata07@gmail.com>
2024-12-12 16:12:29 -0500

    fixed return pages

--- server/controllers/mpController.js
+++ server/controllers/mpController.js
@@ -25,15 +25,15 @@ export const createPreference = async (req, res) => {
       back_urls: {
         success:
           process.env.ENV === "prod"
-            ? "https://g5-frontend.onrender.com/payment_confirmed"
+            ? "https://sdslatam.com/payment_confirmed"
             : "http://localhost:5173/payment_confirmed", // URL de éxito
         failure:
           process.env.ENV === "prod"
-            ? "https://g5-frontend.onrender.com/payment_failed"
+            ? "https://sdslatam.com/payment_failed"
             : "http://localhost:5173/payment_failed", // URL de fallo
         pending:
           process.env.ENV === "prod"
-            ? "https://g5-frontend.onrender.com/payment_pending"
+            ? "https://sdslatam.com/payment_pending"
             : "http://localhost:5173/payment_pending", // URL de pendiente
       },
       auto_return: "approved", // Para que se redirija automáticamente al cliente después de una compra exitosa
