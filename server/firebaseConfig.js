a7ef1b3301409f4a9b7d8b0874b4dfc33cda71ba
Santiago <santiagozapata07@gmail.com>
2024-12-12 00:08:24 -0500

    fix environment issues

--- server/firebaseConfig.js
+++ server/firebaseConfig.js
@@ -1,19 +1,21 @@
 // firebaseConfig.js
 import admin from "firebase-admin";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const firebaseConfig = {
-  type: import.meta.env.VITE_FIREBASE_TYPE,
-  project_id: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  private_key_id: import.meta.env.VITE_FIREBASE_PRIVATE_KEY_ID,
-  private_key: import.meta.env.VITE_FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-  client_email: import.meta.env.VITE_FIREBASE_CLIENT_EMAIL,
-  client_id: import.meta.env.VITE_FIREBASE_CLIENT_ID,
-  auth_uri: import.meta.env.VITE_FIREBASE_AUTH_URI,
-  token_uri: import.meta.env.VITE_FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url: import.meta.env
-    .VITE_FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-  client_x509_cert_url: import.meta.env.VITE_CLIENT_X509_CERT_URL,
-  universe_domain: import.meta.env.VITE_UNIVERSE_DOMAIN,
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+  universe_domain: process.env.UNIVERSE_DOMAIN,
 };
 
 admin.initializeApp({
